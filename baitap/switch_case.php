e7596969e3df8ff5aabd4685bfcb68c2a106e3a1
AnhTai1101 <lecongtai@yahoo.com.vn>
2020-06-08 10:00:12 +0700

    update switch case

--- baitap/switch_case.php
+++ baitap/switch_case.php
@@ -39,168 +39,63 @@
 
 class SwithCaseHandle 
 {
-    public $inputs =["fade",
-    "wipeleft",
-    "wiperight",
-    "wipeup",
-    "wipedown",
-    "slideleft",
-    "slideright",
-    "slideup",
-    "slidedown",
-    "circlecrop",
-    "rectcrop",
-    "distance",
-    "fadeblack",
-    "fadewhite",
-    "radial",
-    "smoothleft",
-    "smoothright",
-    "smoothup",
-    "smoothdown",
-    "circleopen",
-    "circleclose",
-    "vertopen",
-    "vertclose",
-    "horzopen",
-    "horzclose",
-    "dissolve",
-    "pixelize",
-    "diagtl",
-    "diagtr",
-    "diagbl",
-    "diagbr",
-    "hlslice",
-    "hrslice",
-    "vuslice",
-    "vdslice"] ;
+    private $inputsHandle = [];
     //khai bao du lieu mang tren cho class, tai constructor
+
+    /**
+     * Class constructor.
+     */
+    public function __construct()
+    {
+        $this->inputsHandle = ["fade",
+        "wipeleft",
+        "wiperight",
+        "wipeup",
+        "wipedown",
+        "slideleft",
+        "slideright",
+        "slideup",
+        "slidedown",
+        "circlecrop",
+        "rectcrop",
+        "distance",
+        "fadeblack",
+        "fadewhite",
+        "radial",
+        "smoothleft",
+        "smoothright",
+        "smoothup",
+        "smoothdown",
+        "circleopen",
+        "circleclose",
+        "vertopen",
+        "vertclose",
+        "horzopen",
+        "horzclose",
+        "dissolve",
+        "pixelize",
+        "diagtl",
+        "diagtr",
+        "diagbl",
+        "diagbr",
+        "hlslice",
+        "hrslice",
+        "vuslice",
+        "vdslice"];
+    }
+        // $this->inputsHandle  =["fade"=>function($input){
+        //     return $this->fade($input);
+        // }];
+        
+
     
     // voi moi item trong mang da khai bao, can viet 1 function tuong ung de Viet hoa chu cai dau tien
     //vd: voi item "wipeup" se viet ham 
 
     function Handle( $input){
-        // $inputs = $this->inputs;
-        // $test = 0;
-        // foreach($inputs as $val){
-        //     if($val === $input){
-        //         $test++;
-        //     }
-        // }
-        if(!in_array($input, $inputs)){
-            return "Không tồn tại";
+        if(in_array($input,$this->inputsHandle)){
+            return ucfirst($input);
         }
-        switch($input){
-            case 'fade':
-                return $this->fade($input);
-            break;
-            case 'wipeleft':
-                return $this->wipeleft($input);
-            break;
-            case 'wiperight':
-                return $this->wiperight($input);
-            break;
-            case 'wipeup':
-                return $this->wipeup($input);
-            break;
-            case 'wipedown':
-                return $this->wipedown($input);
-            break;
-            case 'slideleft':
-                return $this->slideleft($input);
-            break;
-            case 'slideright':
-                return $this->slideright($input);
-            break;
-            case 'slideup':
-                return $this->slideup($input);
-            break;
-            case 'slidedown':
-                return $this->slidedown($input);
-            break;
-            case 'circlecrop':
-                return $this->circlecrop($input);
-            break;
-            case 'rectcrop':
-                return $this->rectcrop($input);
-            break;
-            case 'distance':
-                return $this->distance($input);
-            break;
-            case 'fadeblack':
-                return $this->fadeblack($input);
-            break;
-            case 'fadewhite':
-                return $this->fadewhite($input);
-            break;
-            case 'radial':
-                return $this->radial($input);
-            break;
-            case 'smoothleft':
-                return $this->smoothleft($input);
-            break;
-            case 'smoothright':
-                return $this->smoothright($input);
-            break;
-            case 'smoothup':
-                return $this->smoothup($input);
-            break;
-            case 'smoothdown':
-                return $this->smoothdown($input);
-            break;
-            case 'circleopen':
-                return $this->circleopen($input);
-            break;
-            case 'circleclose':
-                return $this->circleclose($input);
-            break;
-            case 'vertopen':
-                return $this->vertopen($input);
-            break;
-            case 'vertclose':
-                return $this->vertclose($input);
-            break;
-            case 'horzopen':
-                return $this->horzopen($input);
-            break;
-            case 'horzclose':
-                return $this->horzclose($input);
-            break;
-            case 'dissolve':
-                return $this->dissolve($input);
-            break;
-            case 'pixelize':
-                return $this->pixelize($input);
-            break;
-            case 'diagtl':
-                return $this->diagtl($input);
-            break;
-            case 'diagtr':
-                return $this->diagtr($input);
-            break;
-            case 'diagbl':
-                return $this->diagbl($input);
-            break;
-            case 'diagbr':
-                return $this->diagbr($input);
-            break;
-            case 'hlslice':
-                return $this->hlslice($input);
-            break;
-            case 'hrslice':
-                return $this->hrslice($input);
-            break;
-            case 'vuslice':
-                return $this->vuslice($input);
-            break;
-            case 'vdslice':
-                return $this->vdslice($input);
-            break;
-
-            
-        }
-        
-        //khi dua $input la 1 trong nhung item trong mang khai bao. goi ham tuong ung
     }   
     
     // fade
@@ -347,5 +242,5 @@ class SwithCaseHandle
 
 $test=new SwithCaseHandle();
 
-echo $test->Handle('vdslice');
+echo $test->Handle('fade');
 // ket qua mong cho: Vdslice
